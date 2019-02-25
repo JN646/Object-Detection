2eb7c7d87e77981c6110104fc2ac05441abf6eae
Josh <josh.ginn@hotmail.co.uk>
2019-02-25 22:43:30 +0000

    Flask Demo with random number feed.

---
+++ static/js/application.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    //connect to the socket server.
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+    var numbers_received = [];
+
+    //receive details from server
+    socket.on('newnumber', function(msg) {
+        console.log("Received number: " + msg.number);
+        $('#log').html(msg.number);
+    });
+
+});
