ec25b667fa6f2ddd55947a39e2ecc2c1ce140c24
Josh <josh.ginn@hotmail.co.uk>
2019-05-26 16:25:39 +0100

    Make live object counter OOP.

--- web/classes/class_objectDetection.php
+++ web/classes/class_objectDetection.php
@@ -414,5 +414,30 @@ class objectDetection {
     //now print the data
     print json_encode($total);
   }
+
+  public function liveObjectCounter() {
+    $conn = $this->dbconnect();
+
+    // Get the latest count.
+    $result = $conn->query("SELECT COUNT(*) as count
+    FROM counter
+    WHERE count_time
+    IN (SELECT MAX(count_time) FROM counter)
+    AND count_class = 'person' AND count_deviceID = '2'"
+    );
+
+    // If there are results.
+    if ($result->num_rows > 0) {
+    	while ($row = $result->fetch_assoc()) {
+    		$count = $row['count'];
+
+    		// Output count.
+    		echo "<h1><span>{$count}</span></h1>";
+    	}
+    } else {
+    	// If there are no results.
+    	echo "<h1>N/A</h1>";
+    }
+  }
 }
 ?>
--- web/counter/data.php
+++ web/counter/data.php
@@ -1,30 +1,6 @@
 <?php
-// Make the connection.
-$conn = new mysqli('localhost', 'root', '', 'objectTracker2');
+include '../classes/class_objectDetection.php';
 
-// Connect error catch.
-if ($conn->connect_error) {
-	die("Connection error: " . $conn->connect_error);
-}
-
-// Get the latest count.
-$result = $conn->query("SELECT COUNT(*) as count
-FROM counter
-WHERE count_time
-IN (SELECT MAX(count_time) FROM counter)
-AND count_class = 'person' AND count_deviceID = '2'"
-);
-
-// If there are results.
-if ($result->num_rows > 0) {
-	while ($row = $result->fetch_assoc()) {
-		$count = $row['count'];
-
-		// Output count.
-		echo "<h1><span>{$count}</span></h1>";
-	}
-} else {
-	// If there are no results.
-	echo "<h1>N/A</h1>";
-}
+$foo = new ObjectDetection();
+$foo->liveObjectCounter();
 ?>
